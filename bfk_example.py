ff5c4a416cf35f67d47c9f6ff52bf79079ad8e70
gynamics <44536764+gynamics@users.noreply.github.com>
2021-10-24 22:38:04 +0800

    Update bfk_example.py
    a small change

--- bfk_example.py
+++ bfk_example.py
@@ -10,7 +10,7 @@ i.fuck(     # put your code here
         >++++++++++ inc (2) by 10
         >+++        inc (3) by 3
         >+          inc (4) by 1
-        <<<<-]      while dec (0)
+        <<<<-]      dec (0) by 1 while (0)
         >++.        inc (1) by 2 output 'H'
         >+.         inc (2) by 1 output 'e'
         +++++++.    inc (2) by 7 output 'l'
@@ -20,10 +20,10 @@ i.fuck(     # put your code here
         <<+++++++++++++++.
                     inc (1) by 15 output 'W'
         >.              (2)      output 'o'
-        +++.        add (2) by 3 output 'r'
+        +++.        inc (2) by 3 output 'r'
         ------.     dec (2) by 6 output 'l'
         --------.   dec (2) by 8 output 'd'
-        >+.         add (3) by 1 output '!'
+        >+.         inc (3) by 1 output '!'
         >.              (4)      output '\\n'
         """
 )
